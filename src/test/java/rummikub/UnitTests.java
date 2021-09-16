c3ed984095a009384f4c8719053befb72674492e
alexeikrumshyn <alexei.w.k@gmail.com>
2021-09-16 14:53:44 -0400

    TESTS FOR creation of runs of tiles

--- src/test/java/rummikub/UnitTests.java
+++ src/test/java/rummikub/UnitTests.java
@@ -3,6 +3,8 @@ package rummikub;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+
 public class UnitTests {
 
     @Test
@@ -28,6 +30,59 @@ public class UnitTests {
         assertEquals("4", tile.getNumber());
         assertEquals("table", tile.getSource());
         assertEquals("!|B4|", tile.toString());
+    }
+
+    @Test
+    @DisplayName("creation of runs of tiles")
+    public void createTileRuns() {
+
+        //testing basic run
+        ArrayList<Tile> testRun = new ArrayList<Tile>();
+        testRun.add(new Tile("R","3"));
+        testRun.add(new Tile("R","2"));
+        testRun.add(new Tile("R","5"));
+        testRun.add(new Tile("R","4"));
+        TileCollection run = new TileCollection(testRun);
+        assertTrue(run.isRun());
+        assertFalse(run.isSet());
+        assertTrue(run.isMeld());
+        assertEquals(14,run.getPoints());
+        assertEquals("{ |R2| |R3| |R4| |R5| }", run.toString());
+
+        testRun.remove(1);
+        testRun.add(new Tile("R","7"));
+        assertFalse(run.isRun());
+        assertFalse(run.isSet());
+        assertFalse(run.isMeld());
+        assertEquals(18,run.getPoints());
+        assertEquals("|R2| |R4| |R5| |R7| ", run.toString());
+
+        //testing run with wraparound to Ace (1)
+        ArrayList<Tile> testRun2 = new ArrayList<Tile>();
+        testRun2.add(new Tile("B","11"));
+        testRun2.add(new Tile("B","9"));
+        testRun2.add(new Tile("B","10"));
+        testRun2.add(new Tile("B","1"));
+        testRun2.add(new Tile("B","12"));
+        testRun2.add(new Tile("B","13"));
+        TileCollection run2 = new TileCollection(testRun2);
+        assertTrue(run2.isRun());
+        assertFalse(run2.isSet());
+        assertTrue(run2.isMeld());
+        assertEquals(56,run2.getPoints());
+        assertEquals("{ |B9| |B10| |B11| |B12| |B13| |B1| }", run2.toString());
+
+        //testing run with length less than 3
+        ArrayList<Tile> testRun3 = new ArrayList<Tile>();
+        testRun3.add(new Tile("G","11"));
+        testRun3.add(new Tile("G","10"));
+        TileCollection run3 = new TileCollection(testRun3);
+        assertFalse(run3.isRun());
+        assertFalse(run3.isSet());
+        assertFalse(run3.isMeld());
+        assertEquals(21,run3.getPoints());
+        assertEquals("|G10| |G11| ", run3.toString());
+
     }
 
 }
