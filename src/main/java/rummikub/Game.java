09823d59ff7c6b60ccc3641b34cc456ff0859e5d
alexeikrumshyn <alexei.w.k@gmail.com>
2021-09-17 20:31:25 -0400

    CODE FOR creation of new game

---
+++ src/main/java/rummikub/Game.java
@@ -0,0 +1,47 @@
+package rummikub;
+
+import java.util.ArrayList;
+
+public class Game {
+
+    private TileCollection stock;
+    private ArrayList<TileCollection> table;
+
+    public Game() {
+        createStock();
+        table = new ArrayList<>();
+    }
+
+    /* Creates the initial collection of tiles to draw from */
+    public void createStock() {
+        ArrayList<String> colours = new ArrayList<>();
+        colours.add("B");
+        colours.add("G");
+        colours.add("O");
+        colours.add("R");
+
+        stock = new TileCollection();
+        for (String clr : colours) {
+            for (int i = 1; i <= 13; ++i) {
+                stock.add(new Tile(clr,Integer.toString(i)));
+            }
+        }
+    }
+
+    /* Returns a string representation of the stock */
+    public String getStock() {
+        return stock.toString();
+    }
+
+    /* Returns a string representation of the table */
+    public String getTable() {
+
+        String str = "";
+        for (TileCollection c : table) {
+            str += c.toString();
+            str += "\n";
+        }
+        return str;
+    }
+
+}
